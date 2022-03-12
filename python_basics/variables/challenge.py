c8f65df21866aebb61ff0a615637680bf37ea261
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-12 10:29:17 +0200

    Changing variable values Challenge to complete

---
+++ python_basics/variables/challenge.py
@@ -0,0 +1,16 @@
+# Create a program to assign one variable to other.
+
+# • Create a variable named favorite_food with the value 'steak'.
+favorite_food = 'steak'
+
+# • Print the favorite_food variable.
+print(favorite_food)
+
+# • Create another variable named food with the value 'pizza'.
+food = 'pizza'
+
+# • Assign the food variable to the favorite_food variable.
+favorite_food = food
+
+# • Print the favorite_food variable again.
+print(favorite_food)
