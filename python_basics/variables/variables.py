ea5f7b1f5eb3ba25024eaa4802c8880b887d89c6
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-12 10:14:19 +0200

    How to create a variable

--- python_basics/variables/variables.py
+++ python_basics/variables/variables.py
@@ -8,6 +8,6 @@ name = 'John'  # string
 
 # printing the variableStoring data allows us to use the same data later in the program. For this, we use something called variables.
 
-print(x)  # output: 25
+print(x)  # Output: 25
 
-print(name)  # output: John
+print(name)  # Output: John
