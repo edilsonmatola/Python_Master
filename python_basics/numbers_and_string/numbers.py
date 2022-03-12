c7519675345710e844435c6377b3c3ee6f079660
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-12 09:49:18 +0200

    Basic files to Intro Python

---
+++ python_basics/numbers_and_string/numbers.py
@@ -0,0 +1,19 @@
+# Integers (int) are numbers without decimal parts. For example, 5, -11, 0, 12, etc.
+int_1 = 5
+int_2 = -11
+int_3 = 0
+int_4 = 12
+int_5 = 13454
+
+# Floating-point (float) numbers contain decimal parts. For example, 2.5, 6.76, 0.0, -9.45, etc.
+float_1 = 2.5
+float_2 = 6.75
+float_3 = 0.0
+float_4 = -9.45
+float_5 = 13.98
+
+# Printing Numbers
+print(65)
+
+# Printing multiple numbers
+print(int_1, float_3)
---
+++ python_basics/numbers_and_string/string.py
@@ -0,0 +1,17 @@
+# Strings (str) are texts wrapped inside quotation marks.
+
+string_1 = 'This is a string.'
+
+# We can also use matching double quotes to create strings.
+
+string_2 = "This is also a string"
+
+# Printing Strings
+
+print('Hi there!')
+
+print(string_1)
+
+# Printing multiple strings
+
+print('Hey.', 'Are you enjoying the repo?', 'Give it a star and follow me.')
