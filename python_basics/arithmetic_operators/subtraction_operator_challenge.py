ec64852236bd6b0c8e4322e445adef5ab0a20acf
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-13 22:20:24 +0200

    Add Subtraction Operator Challenge

---
+++ python_basics/arithmetic_operators/subtraction_operator_challenge.py
@@ -0,0 +1,19 @@
+"""
+* Problem Description
+
+Suppose the cost price of a book is 25 dollars. 
+Store it in a variable named cost_price.
+
+The selling price of the book is 35 dollars. 
+Store it in a variable named selling_price.
+
+Calculate the profit amount on the book.
+"""
+
+cost_price = 25
+
+selling_price = 35
+
+profit = selling_price - cost_price
+
+print('profit:', profit)
