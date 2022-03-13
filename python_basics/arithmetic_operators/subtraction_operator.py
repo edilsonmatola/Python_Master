c2d28dee630281c19bb708031270e485b9a4bfbb
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-13 22:17:58 +0200

    Add Subtraction  Operator

---
+++ python_basics/arithmetic_operators/subtraction_operator.py
@@ -0,0 +1,23 @@
+# The - operator subtracts the value on the right from the value on the left.
+# Example:
+
+x = 10
+
+y = 5
+
+# Using - to perfom subtraction
+result = x - y
+
+print(result)   # Output: 5
+
+
+# Example 2:
+
+w = 50
+
+z = 10
+
+# Using - to perfom subtraction
+result2 = z - w
+
+print(result2)  # Output: -40
