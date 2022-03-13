844948e8c4cd2c7798c63edf5f88a4acf4fabf27
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-13 21:33:14 +0200

    Add Plus Operator

---
+++ python_basics/arithmetic_operators/plus_operator.py
@@ -0,0 +1,21 @@
+# (+) operator is used to add operands (values and variables).
+
+x = 10
+
+y = 20
+
+
+# Using + to perfom addition
+
+total = x + y + 30
+
+print(total)   # Output: 60
+
+
+# Example 2:
+
+w = 22.5
+
+z = 12.5
+
+print(w + z)  # Output: 35
