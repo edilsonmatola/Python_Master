9459bc0ee59fbc3c77128bc0c342946465c69ac9
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-14 14:47:25 +0200

    Added Multiplication Operator

---
+++ python_basics/arithmetic_operators/multiplication_operator.py
@@ -0,0 +1,18 @@
+#  The * operator multiplies two values/variables.
+
+number1 = 5
+
+number2 = 2
+
+# computing the product of number1 and number2
+product = number1 * number2
+
+print(product)  # Output: 10
+
+# Example 2:
+
+number3 = 10
+
+product2 = number3 * 10
+
+print(product2)  # Output: 100
