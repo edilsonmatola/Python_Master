f465311193da1cfb155f77bd9e4a355dcbc95b8d
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-14 14:53:46 +0200

    Added Division Operator

---
+++ python_basics/arithmetic_operators/division_operator.py
@@ -0,0 +1,30 @@
+# The / operator is used for division. For example,
+
+number = 20
+
+result = number / 2
+
+print(result)   # Output: 10.0
+
+# Example 2
+
+number2 = 25
+
+result2 = number2 / 5
+
+print(result2)  # Output: 5.0
+
+
+# Example 3
+
+number3 = 35
+
+result3 = number3 / 4
+
+print(result3)  # Output: 8.75
+
+
+"""
+* Note: The / operator always returns a float value. 
+* In the example 2 program 25 / 5 results in 5.0 and not 5.
+"""
