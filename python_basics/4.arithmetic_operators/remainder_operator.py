6d12b076f293e37fa7eeec7d938bbad67f48a206
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-15 09:41:09 +0200

    Remainder Operator file

---
+++ python_basics/4.arithmetic_operators/remainder_operator.py
@@ -0,0 +1,21 @@
+# The / operator performs division. 
+# However, if we want to find the quotient instead, we use the // operator.
+
+divisor = 4
+dividend = 25
+
+# compute quotient
+quotient = dividend // divisor
+
+print('Quotient:', quotient)    # Output: 6
+
+
+# The % operator computes remainder when one number is divided by another.
+
+divisor = 4
+dividend = 25
+
+# compute remainder
+remainder = dividend % divisor
+
+print('Remainder:', remainder)    # Output: 1
