624df1b4cbc53064658955692d10e22ed26a6376
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-18 22:12:34 +0200

    Implicit Type Conversion

---
+++ python_basics/5.data_conversion/implicit_type_conversion.py
@@ -0,0 +1,9 @@
+# Sometimes, Python automatically converts data of one type to another. For example,
+
+number_integer = 123
+number_float = 1.23
+
+# adding integer and float
+result = number_integer + number_float
+
+print(result)   # Output: 124.23
