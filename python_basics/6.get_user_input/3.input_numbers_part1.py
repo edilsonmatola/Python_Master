5b9a8e1d8e009c25d2bcd5d348eff3407c9d60a8
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-18 22:38:19 +0200

    Converting User Input into a Number part 1

---
+++ python_basics/6.get_user_input/3.input_numbers_part1.py
@@ -0,0 +1,10 @@
+# One important thing to remember with the input() function is that it always takes input in the form of a string.
+
+# Program to multiply numbers entered by the user
+
+# take inputs the user
+number1 = input('Enter a number: ')
+number2 = input('Enter another number: ')
+
+product = number1 * number2
+print('Product:', product)  # Error
