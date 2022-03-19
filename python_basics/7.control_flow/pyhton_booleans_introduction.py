99cb1d3f5a53079250086d89c591d8dad0fe96c0
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-19 21:31:20 +0200

    Introduction to Python Booleans

---
+++ python_basics/7.control_flow/pyhton_booleans_introduction.py
@@ -0,0 +1,17 @@
+# Booleans
+# Booleans represent one of two possible values: True or False. Let's take an example.
+
+result1 = True
+result2 = False
+
+print(result1)  # Output: True
+print(result2)  # Output: False
+
+"""
+* There are a few things we need to understand about booleans:
+
+* True is a boolean. However, if we use True inside quotation marks, 'True', it is a string. It's the same case * for False and 'False'.
+* True and False are not variables. In fact, we cannot create variables named True and False.
+* True and False must start with a capital letter.
+
+"""
