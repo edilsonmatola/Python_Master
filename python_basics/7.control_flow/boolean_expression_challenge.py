6776310d1caa42efa920c0f6c31656f68bdbbebf
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-19 21:31:56 +0200

    Python Booleans Challenge

---
+++ python_basics/7.control_flow/boolean_expression_challenge.py
@@ -0,0 +1,15 @@
+"""
+* Problem Description
+
+* Program to print True if the number is divisible by 5. If not, print False.
+
+* Get an integer input from the user.
+* Check if the input is divisible by 5 or not. If it's divisible by 5, print True. If not, print False.
+
+"""
+
+# Get an integer input from the user
+number = int(input('Insert a number: '))
+
+# Check if the input is divisible by 5 or not. If it's divisible by 5, print True. If not, print False.
+print(number % 5 == 0)
