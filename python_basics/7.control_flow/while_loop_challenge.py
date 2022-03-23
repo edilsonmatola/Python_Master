695a2da36ee88705188385125dd7d321a660795c
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-23 15:57:36 +0200

    Python while loop First  challenge

---
+++ python_basics/7.control_flow/while_loop_challenge.py
@@ -0,0 +1,17 @@
+"""
+
+* Create a program to print numbers from 5 to 1 using a while loop.
+
+* Create variable i with value 5.
+
+* Inside a while loop, print the value of i.
+
+* Decrease the value of i by 1 in each iteration of the loop.
+
+"""
+
+i = 5
+
+while i > 0:
+    print('i = ', i)
+    i = i - 1
