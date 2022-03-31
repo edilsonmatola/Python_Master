577898560c227a9f1e0678c426d7662c097fbbf0
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-31 08:00:25 +0200

    Pyhton For loop

---
+++ python_basics/7.control_flow/for_loop.py
@@ -0,0 +1,22 @@
+name = 'Edilson'
+
+for letter in name:
+    print(letter)
+
+
+# For Loop with List
+
+countries = ['Mozambique', 'Portugal', 'France', 'Brasil']
+
+for country in countries:
+    print(country)
+
+
+# For Loop with range
+
+
+# range(1, 5) a sequence of numbers 1, 2, 3 and 4
+# Note: 10 is exclusive
+
+for i in range(1, 10):
+    print(i)
